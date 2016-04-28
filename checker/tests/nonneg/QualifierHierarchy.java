4195c966bfbd0852f1e40968bef86d4c94bfc2ca
Grant Y Hughes <granthughes93@gmail.com>
2016-04-28 16:46:19 -0700

    Creating test cases for the nonneg checker.

--- checker/tests/nonneg/QualifierHierarchy.java
+++ checker/tests/nonneg/QualifierHierarchy.java
@@ -1,10 +1,23 @@
-import Trivial.qual.*;
-import org.checkerframework.checker.nonneg.qual.nonneg;
+import org.checkerframework.checker.nonneg.*;
+import org.checkerframework.checker.nonneg.qual.*;
 
-@Trivial.qual.DefaultQualifier(Unknown.class)
-public class ArrayArgs {
+//@Trivial.qual.DefaultQualifier(Unknown.class)
 
-    public void test(@NonNegative int i) {
+
+/*
+ * These test cases test to make sure that the qualifier hierarchy is
+ * correctly enforced using inline annotations.
+ */
+public class QualifierHierarchy {
+
+    void testImplicitDecloration() {
+    	@NonNegative int a = 1;
+    	@NonNegative int b = 2;
+    	@Unknown int c = -1;
+    	@Unknown int d = 3;
+    	
+        //:: error: (assignment.type.incompatible)
+    	@NonNegative int e = -1;
     }
 
     public void test() {
