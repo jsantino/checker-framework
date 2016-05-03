1fd6379f90f6dae180a0e04f40e43006b43752bb
jsantino <jsant@DESKTOP-D01B8VN>
2016-05-02 21:42:48 -0700

    add quals import to tests

--- checker/tests/index-general/TypeRuleIndexOrLowB.java
+++ checker/tests/index-general/TypeRuleIndexOrLowB.java
@@ -1,7 +1,9 @@
+import org.checkerframework.checker.index.qual.*;
+
 class TypeRuleIndexOrLowB {
   
-	int arr = new int[5];
-	int arrB = new int[5];
+	int[] arr = new int[5];
+	int[] arrB = new int[5];
 	
 	// TODO: define error
 	//:: error :
