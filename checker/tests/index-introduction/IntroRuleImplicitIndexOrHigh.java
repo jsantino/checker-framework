ec69cef2d9f221b17f46bf7d8274074449a36f76
Kevin Klein <kleinkevin.lu@gmail.com>
2016-05-03 14:59:52 -0700

    added implicit introduction rule tests

---
+++ checker/tests/index-introduction/IntroRuleImplicitIndexOrHigh.java
@@ -0,0 +1,28 @@
+import org.checkerframework.checker.index.qual.*;
+
+//Doesn't test anything yet.
+
+class IntroRuleImplicitIndexOrHigh {
+	
+	int[] arr = new int[5];
+	
+	void foo() {
+		
+		// int v1 = 0; This case is to be discusses. Unsure whether 0 should be IndexOrHigh or NonNegative.
+		int v2 = 1;
+		int v2 = arr.length;
+		
+		// Idea: prove type of variable v by assigning it to a variable of type t and assigning a variable of type t to v
+		@IndexOrHigh("arr") int indexOrHigh1 = arr.length;
+		@IndexorHigh("arr") int indexOrHigh2 = arr.length;
+		
+		// Prove type of v1,v2 is subtype of IndexOrHigh
+		indexOrHigh1 = v1;
+		indexOrHigh1 = v2;
+		
+		// Prove NonNegative is subtype of type of v1,v2
+		v1 = indexOrHigh2;
+		v2 = indexOrHigh2;
+		
+	}
+}
---
+++ checker/tests/index-introduction/IntroRuleImplicitNonNegative.java
@@ -0,0 +1,24 @@
+import org.checkerframework.checker.index.qual.*;
+
+class IntroRuleImplicitNonNegative {
+	
+	void foo() {
+		
+		// int v1 = 0; This case is to be discusses. Unsure whether 0 should be IndexOrHigh or NonNegative.
+		int v2 = 1;
+		int v3 = Integer.MAX_VALUE;
+		
+		// Idea: prove type of variable v by assigning it to a variable of type t and assigning a variable of type t to v
+		@NonNegative int nn1 = 10;
+		@NonNegative int nn2 = 10;
+		
+		// Prove type of v1,v2 is subtype of NonNegative
+		nn1 = v1;
+		nn1 = v2;
+		
+		// Prove NonNegative is subtype of type of v1,v2
+		v1 = nn2;
+		v2 = nn2;
+		
+	}
+}
