56317a51bdef9b23f85e58548c31781ac9a1b265
jsantino <jsantino@bpm.cs.washington.edu>
2016-05-05 16:07:53 -0700

    move resultStore to visitGreaterThan from its callees

--- checker/src/org/checkerframework/checker/index/IndexTransfer.java
+++ checker/src/org/checkerframework/checker/index/IndexTransfer.java
@@ -18,6 +18,7 @@ import org.checkerframework.framework.flow.CFAbstractTransfer;
 import org.checkerframework.framework.flow.CFStore;
 import org.checkerframework.framework.flow.CFValue;
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
+import org.checkerframework.javacutil.AnnotationUtils;
 
 
 public class IndexTransfer extends CFAbstractTransfer<CFValue, CFStore, IndexTransfer> {
@@ -51,35 +52,55 @@ public class IndexTransfer extends CFAbstractTransfer<CFValue, CFStore, IndexTra
 		Node right = node.getRightOperand();
 		Receiver rec = FlowExpressions.internalReprOf(analysis.getTypeFactory(), left);
 		AnnotatedTypeMirror leftType = atypeFactory.getAnnotatedType(left.getTree());
-		if(leftType.hasAnnotation(Unknown.class)){
-			return UnknownGreaterThan(rec,right, result);
-		}
-
-
-		return result;
-	}
-	//********************************************************************************//
-	// these are methods for GreaterThan Nodes once left operand Annotation is known  //
-	//********************************************************************************//
-	// this returns a transfer result for @Unknown > x
-	private TransferResult<CFValue, CFStore> UnknownGreaterThan(Receiver rec, Node right, TransferResult<CFValue, CFStore> result){
+		
 		CFStore thenStore = result.getRegularStore();
 		CFStore elseStore = thenStore.copy();
 		ConditionalTransferResult<CFValue, CFStore> newResult = 
 				new ConditionalTransferResult<>(result.getResultValue(), thenStore, elseStore);
+		
+		if(leftType.hasAnnotation(Unknown.class)){
+			UnknownGreaterThan(rec,right, thenStore);
+		}
+		if(leftType.hasAnnotation(IndexOrLow.class)){
+			//return IndexOrLowGreaterThan(rec, right, thenStore);
+		}
+
+
+		return newResult;
+	}
+
+	//********************************************************************************//
+	// these are methods for GreaterThan Nodes once left operand Annotation is known  //
+	//********************************************************************************//
+	// this returns a transfer result for @Unknown > x
+	private void UnknownGreaterThan(Receiver rec, Node right, CFStore thenStore){
 		AnnotatedTypeMirror rightType = atypeFactory.getAnnotatedType(right.getTree());
 		// booleans to see if the type is any in the heirarchy we want to refine
 		boolean IOL = rightType.hasAnnotation(IndexOrLow.class);
 		boolean NN = rightType.hasAnnotation(NonNegative.class);
 		boolean IOH = rightType.hasAnnotation(IndexOrHigh.class);
 		boolean IF = rightType.hasAnnotation(IndexFor.class);
-		//
 		if(IOL || NN || IOH || IF){
 			AnnotationMirror anno = atypeFactory.createNonNegAnnotation();
 			thenStore.insertValue(rec, anno);
-			return newResult;
 		}
-		return result;
 	}
+	
+//	private TransferResult<CFValue, CFStore> IndexOrLowGreaterThan(Receiver rec, Node right,
+//			TransferResult<CFValue, CFStore> result) {
+//		CFStore thenStore = result.getRegularStore();
+//		CFStore elseStore = thenStore.copy();
+//		ConditionalTransferResult<CFValue, CFStore> newResult = 
+//				new ConditionalTransferResult<>(result.getResultValue(), thenStore, elseStore);
+//		AnnotatedTypeMirror rightType = atypeFactory.getAnnotatedType(right.getTree());
+//		for(AnnotationMirror anno: rightType.getAnnotations()){
+//			boolean IOL = AnnotationUtils.areSameIgnoringValues(anno, atypeFactory.IndexOrLow);
+//			boolean InF = AnnotationUtils.areSameIgnoringValues(anno, atypeFactory.IndexFor);
+//			if(IOL || InF){
+//				String aValue = IndexVisitor.getIndexValue(anno, atypeFactory.getValueMethod(anno));
+//			}
+//		}
+//	}
+
 }
 
