2a765497f62e379591b6cc604c89b78265bed8a6
jsantino <jsantino@agraf.cs.washington.edu>
2016-05-06 13:53:08 -0700

    add test back now that we have subtraction

--- checker/tests/index/index-introduction/IntroRuleImplicitIndexOrLow.java
+++ checker/tests/index/index-introduction/IntroRuleImplicitIndexOrLow.java
@@ -11,12 +11,12 @@ class IntroRuleImplicitIndexOrHigh {
 		int v1 = -1; 
 		
 		// Idea: prove type of variable v by assigning it to a variable of type t and assigning a variable of type t to v
-		//@IndexOrLow("arr") int indexOrLow1 = arr.length -1;
-		//@IndexOrLow("arr") int indexOrLow2 = arr.length -1;
+		@IndexOrLow("arr") int indexOrLow1 = arr.length -1;
+		@IndexOrLow("arr") int indexOrLow2 = arr.length -1;
 		
 		// Prove type of v1 is subtype of IndexOrLow
-		@IndexOrLow("") int indexOrLow1 = v1;
-		@IndexOrLow("") int indexOrLow2 = -1;
+		//@IndexOrLow("") int indexOrLow1 = v1;
+		//@IndexOrLow("") int indexOrLow2 = -1;
 		// Prove IndexOrLow is subtype of type of v1
 		v1 = indexOrLow2;
 		
