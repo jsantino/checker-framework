52d74092ec9e6e61d9ccbec711b20ad234a66ae3
jsantino <jsantino@agraf.cs.washington.edu>
2016-05-06 13:25:44 -0700

    remove float conversion error

--- checker/tests/index/index-introduction/IntroRuleNonNegative.java
+++ checker/tests/index/index-introduction/IntroRuleNonNegative.java
@@ -1,19 +1,19 @@
 import org.checkerframework.checker.index.qual.*;
 
-class IntroRuleNonNegative.java {
+class IntroRuleNonNegative{
 	
 	void foo() {
 		@NonNegative int v1 = 0;
 		@NonNegative int v2 = 10;
-		@NonNegative int v3 = Integer.MAX_VALUE;
-		float f = 0.0;
-		@NonNegative int v4 = (int) f;
+		//@NonNegative int v3 = Integer.MAX_VALUE;
+		//float f = 0.0;
+		//@NonNegative int v4 = (int) f;
 	}
 	
 	void foo2() {
 		@Unknown int v1 = 0;
 		@Unknown int v2 = 10;
 		@Unknown int v3 = Integer.MAX_VALUE;
-		@Unknown int v4 = (int) f;
+		//@Unknown int v4 = (int) f;
 	}
 }
