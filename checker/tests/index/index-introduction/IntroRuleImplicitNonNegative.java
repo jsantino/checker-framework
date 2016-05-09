fe6d37741f0011b51b36a05db91ab41445a5aab3
kkleindev <kleinkevin.lu@gmail.com>
2016-05-09 11:58:02 -0700

    replaced introduction of Integer.max_value by arbitrary large integer as max_value is a field

--- checker/tests/index/index-introduction/IntroRuleImplicitNonNegative.java
+++ checker/tests/index/index-introduction/IntroRuleImplicitNonNegative.java
@@ -6,8 +6,8 @@ class IntroRuleImplicitNonNegative {
 		
 		// int v1 = 0; This case is to be discusses. Unsure whether 0 should be IndexOrHigh or NonNegative.
 		int v1 = 1;
-		// Integer.Max_Value is not an int literal it is a field...
-		//int v2 = Integer.MAX_VALUE;
+		// Integer.Max_Value cannot be used here as it is only known at run-time
+		int v2 = 10000;
 		
 		// Idea: prove type of variable v by assigning it to a variable of type t and assigning a variable of type t to v
 		@NonNegative int nn1 = 10;
@@ -15,11 +15,11 @@ class IntroRuleImplicitNonNegative {
 		
 		// Prove type of v1,v2 is subtype of NonNegative
 		nn1 = v1;
-		//nn1 = v2;
+		nn1 = v2;
 		
 		// Prove NonNegative is subtype of type of v1,v2
 		v1 = nn2;
-		//v2 = nn2;
+		v2 = nn2;
 		
 	}
 }
